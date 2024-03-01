26dd62831f2e15fe2ff4cd12f35160e37fe85e55
Liker <79975604+AstonishedLiker@users.noreply.github.com>
2024-03-01 16:46:17 +0100

    Update LockPicking.cs

--- ERLC/Robberies/LockPicking.cs
+++ ERLC/Robberies/LockPicking.cs
@@ -1,44 +1,43 @@
 using System.Drawing;
 
 namespace ERLC.Robberies;
-
 public class LockPicking
 {
-    const int StartTime = 1;
+    private const int StartTime = 1;
     private static Color LineColor = ColorTranslator.FromHtml("#FFC903");
-    
+
     public static void StartProcess()
     {
-        Console.WriteLine($"i ~ Starting process in {StartTime.ToString()}");
+        Console.WriteLine($"i ~ Starting process in {StartTime}");
         Roblox.FocusRoblox();
-        Thread.Sleep(StartTime * 1000);
-        
-        var (linePosX, linePosY) = Screen.LocateColorInScreen(LineColor, 0);
-        var mLinePosX = linePosX + 570;
 
+        Thread.Sleep(StartTime * 1000);
+
+        var (linePosX, linePosY) = Screen.LocateColor(LineColor, 0);
         if (linePosX == 0 && linePosY == 0)
         {
-            Console.WriteLine("! ~ LockPicking line could not be found !");
+            Console.WriteLine("! ~ LockPicking line could not be found!");
             return;
         }
-        
-        Console.WriteLine($"i ~ Found Line at {linePosY},{linePosY}");
-        
-        for (var rectI = 1; rectI < 7; rectI ++) {
-            var x = linePosX + (83 * rectI);
-            
+
+        Console.WriteLine($"i ~ Found Line at {linePosY}, {linePosY}");
+
+        for (int rectI = 1; rectI < 7; rectI++)
+        {
+            int x = linePosX + (83 * rectI);
+
             while (true)
             {
-                var color1 = Screen.GetColorAtPixel(x, linePosY + 10);
-                var color2 = Screen.GetColorAtPixel(x, linePosY - 4);
+                Color color1 = Screen.GetColorAtPixel(x, linePosY + 10);
+                Color color2 = Screen.GetColorAtPixel(x, linePosY - 4);
 
                 if (
                     color1.R > 140 & color1.G > 140 & color1.B > 140 &&
                     color2.R > 140 & color2.G > 140 & color2.B > 140
-                ) {
-                    Mouse.Mouse1Down();
-                    Mouse.Mouse1Down();
-                    
+                )
+                {
+                    Mouse.LeftClick();
+
                     Mouse.SetMousePos(x, linePosY);
                     break;
                 }
